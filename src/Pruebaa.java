590c9233412482c98aabf843bffb66cd2e74f80c
Pablo <pablovb@hotmail.com>
2022-02-09 17:34:52 +0100

    Segundo commit

--- src/Pruebaa.java
+++ src/Pruebaa.java
@@ -2,6 +2,7 @@
 public class Pruebaa {
 	 public static void main(String args[])
 	    {
-	        System.out.println("GeeksforGeeks");
+	        System.out.println("Funciona");
+	        
 	    }
 }
