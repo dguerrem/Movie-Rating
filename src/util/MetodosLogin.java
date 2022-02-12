55f3ec76928ae5a6f416faf8d071421e51206fa3
dguerrem <dacormo01@hotmail.com>
2022-02-12 16:05:31 +0100

    .
    .

--- src/util/MetodosLogin.java
+++ src/util/MetodosLogin.java
@@ -1,37 +1,32 @@
 package util;
 
-
 import javax.swing.JOptionPane;
-
 
 public class MetodosLogin {
 
 	public static String transformarContraseña(String password) {
-		
+
 		String passAsteriscos = "";
 		for (int i = 0; i < password.length(); i++) {
 			passAsteriscos += "*";
 		}
 		return passAsteriscos;
 	}
-	
+
 	public static void iniciarSesion(String usuario, String password) {
 		String error = "";
 		System.out.println(usuario + "\n" + password);
-		
+
 		if (usuario.isEmpty() || password.isEmpty()) {
 			error = "Faltan datos";
 		}
-		//Hay que mirar que condiciones ponemos para que se puedan logear ej(contraseña inferior a 8 caracteres ...)
-		
-		
-		//Hay que hacer un select a la BBDD y comprobar que el nombre de usuario este si esta hay que comparar si la contraseña es igual
-		
+		// Hay que mirar que condiciones ponemos para que se puedan logear ej(contraseña
+		// inferior a 8 caracteres ...)
+
+		// Hay que hacer un select a la BBDD y comprobar que el nombre de usuario este
+		// si esta hay que comparar si la contraseña es igual
+
 		JOptionPane.showMessageDialog(null, error, "Titulo", 0);
 	}
-	
-	.
-	
-	
-	
+
 }
