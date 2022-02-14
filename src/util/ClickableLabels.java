f2349f31d4987a3196a1100be7aafeed38987a53
dguerrem <dacormo01@hotmail.com>
2022-02-14 12:56:37 +0100

    Clickable Label

---
+++ src/util/ClickableLabels.java
@@ -0,0 +1,41 @@
+package util;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.JLabel;
+
+public class ClickableLabels extends JLabel {
+
+	private static final long serialVersionUID = 1L;
+
+	public ClickableLabels() {
+		this("");
+	}
+
+	public ClickableLabels(String label) {
+		super(label);
+
+		setForeground(Color.BLUE.darker());
+		setCursor(new Cursor(Cursor.HAND_CURSOR));
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		g.setColor(Color.blue);
+
+		Insets insets = getInsets();
+
+		int left = insets.left;
+		if (getIcon() != null) {
+			left += getIcon().getIconWidth() + getIconTextGap();
+		}
+
+		g.drawLine(left, getHeight() - 1 - insets.bottom, (int) getPreferredSize().getWidth() - insets.right,
+				getHeight() - 1 - insets.bottom);
+	}
+
+}
