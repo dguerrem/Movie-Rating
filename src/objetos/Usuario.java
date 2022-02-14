82e5d34d961b66c36e4428ddfba596001319f8fe
Pablo <pablovb@hotmail.com>
2022-02-14 18:13:32 +0100

    Identado

--- src/objetos/Usuario.java
+++ src/objetos/Usuario.java
@@ -5,98 +5,82 @@ import java.sql.Date;
 public class Usuario {
 
 	String nombre;
-	String apellido1;
-	String apellido2;
+	String apellidoUno;
+	String apellidoDos;
 	Date fechaNacimiento;
 	String correo;
 	String password;
-	String numeroVerificacion;
-	
+	String codVerificacion;
+
 	public Usuario() {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-	public Usuario(String nombre, String apellido1, String apellido2, Date fechaNacimiento, String correo, String password, String numeroVerificacion) {
+
+	public Usuario(String nombre, String apellido1, String apellido2, Date fechaNacimiento, String correo,
+			String password, String numeroVerificacion) {
 		this.nombre = nombre;
-		this.apellido1 = apellido1;
-		this.apellido2 = apellido2;
+		this.apellidoUno = apellido1;
+		this.apellidoDos = apellido2;
 		this.fechaNacimiento = fechaNacimiento;
 		this.correo = correo;
 		this.password = password;
-		this.numeroVerificacion = numeroVerificacion;
+		this.codVerificacion = numeroVerificacion;
 	}
-
 
 	public String getNombre() {
 		return nombre;
 	}
 
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-
 	public String getApellido1() {
-		return apellido1;
+		return apellidoUno;
 	}
-
 
 	public void setApellido1(String apellido1) {
-		this.apellido1 = apellido1;
+		this.apellidoUno = apellido1;
 	}
-
 
 	public String getApellido2() {
-		return apellido2;
+		return apellidoDos;
 	}
-
 
 	public void setApellido2(String apellido2) {
-		this.apellido2 = apellido2;
+		this.apellidoDos = apellido2;
 	}
-
 
 	public Date getFechaNacimiento() {
 		return fechaNacimiento;
 	}
 
-
 	public void setFechaNacimiento(Date fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
-
 
 	public String getCorreo() {
 		return correo;
 	}
 
-
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
-
 
 	public String getPassword() {
 		return password;
 	}
 
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
-
 	public String getNumeroVerificacion() {
-		return numeroVerificacion;
+		return codVerificacion;
 	}
-
 
 	public void setNumeroVerificacion(String numeroVerificacion) {
-		this.numeroVerificacion = numeroVerificacion;
+		this.codVerificacion = numeroVerificacion;
 	}
-	
-	
-	
+
 }
--- src/util/MetodosRegistroUsuario.java
+++ src/util/MetodosRegistroUsuario.java
@@ -5,78 +5,78 @@ import javax.swing.JOptionPane;
 import objetos.Usuario;
 
 public class MetodosRegistroUsuario {
-	
-	
-	//Metodo que tiene que llamarse al pulsar registrar usuario
-	public static void comprobarCampos (String nombre, String apellido1, String apellido2, Date fechaNacimiento, String correo, String password, String password2) {
-		
+
+	// Metodo que tiene que llamarse al pulsar registrar usuario
+	public static void comprobarCampos(String nombre, String apellido1, String apellido2, Date fechaNacimiento,
+			String correo, String password, String password2) {
+
 		String error = "";
-		
-		if(nombre.isEmpty() || nombre.length()<8) {
+
+		if (nombre.isEmpty() || nombre.length() < 8) {
 			error += "Nombre ";
 		}
-		
-		if(apellido1.isEmpty() || apellido2.isEmpty()) {
+
+		if (apellido1.isEmpty() || apellido2.isEmpty()) {
 			error += "Apellidos ";
 		}
-		
+
 		if (fechaNacimiento.equals(null)) {
 			error += "Fecha de nacimiento ";
 		}
-		
-		if (password.isEmpty() || password.length()<8) {
+
+		if (password.isEmpty() || password.length() < 8) {
 			error += "Contraseña ";
 		}
-		
+
 		if (password != password2) {
 			error = "Las contraseñas no son iguales";
 		}
-		
+
 		if (!error.isEmpty()) {
-			JOptionPane.showMessageDialog(null, "Faltan datos o datos incorectos en los siguientes campos: " + error, "Faltan datos", 0);
+			JOptionPane.showMessageDialog(null, "Faltan datos o datos incorectos en los siguientes campos: " + error,
+					"Faltan datos", 0);
 		}
-		if (revisarCorreo(correo) && error.isEmpty()){
+		if (revisarCorreo(correo) && error.isEmpty()) {
 			String numeroVerificacion = generarNumeroVerificacion();
-			Usuario usu = new objetos.Usuario(nombre, apellido1, apellido2, fechaNacimiento, correo, password, numeroVerificacion);
+			Usuario usu = new objetos.Usuario(nombre, apellido1, apellido2, fechaNacimiento, correo, password,
+					numeroVerificacion);
 			guardarUsuarioBBDD(usu);
 			JOptionPane.showMessageDialog(null, "Revise su correo", "Usuario creado correctamente", 0);
 		}
-			
+
 	}
-	
-	
+
 	public static String generarNumeroVerificacion() {
-		
+
 		String letras = "abcdfghijklmnpqrstuvwxyz";
 		String letrasMayus = letras.toUpperCase();
 		String numeros = "0123456789";
-		String todos = letras+letrasMayus+numeros;
-		String numeroVerificacion="";
-		
+		String todos = letras + letrasMayus + numeros;
+		String numeroVerificacion = "";
+
 		for (int i = 0; i < 6; i++) {
-			int numero = (int)(Math.random()*todos.length()+1);
-			numeroVerificacion += todos.substring(numero -1,numero);
+			int numero = (int) (Math.random() * todos.length() + 1);
+			numeroVerificacion += todos.substring(numero - 1, numero);
 		}
-		
-		return numeroVerificacion; 
+
+		return numeroVerificacion;
 	}
-	
-	public static void guardarUsuarioBBDD (Usuario usu) {
-		//Hay que implementar la logica para guardar un objeto en la BBDD, si no se puede hay que crear los metodos get en usuario.
-		
+
+	public static void guardarUsuarioBBDD(Usuario usu) {
+		// Hay que implementar la logica para guardar un objeto en la BBDD, si no se
+		// puede hay que crear los metodos get en usuario.
+
 	}
-	
+
 	public static boolean revisarCorreo(String correo) {
-		
+
 		for (int i = 0; i < correo.length(); i++) {
-			if(correo.charAt(i)=='@') {
+			if (correo.charAt(i) == '@') {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
 
 }
