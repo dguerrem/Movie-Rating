26c3bc4caabeac9c529efe512875e235e886a02f
dguerrem <dacormo01@hotmail.com>
2022-02-28 21:20:50 +0100

    Estructura básica para envío de mails
    Se adjunta la nueva estructura del proyecto que permite enviar mensajes mail sin problema alguno.

--- src/aaMain/Main.java
+++ src/aaMain/Main.java
@@ -1,9 +1,7 @@
 package aaMain;
 
 import java.sql.SQLException;
-
 import javax.swing.UnsupportedLookAndFeelException;
-
 import interfaz.PanelPrincipalLogin;
 
 public class Main {
@@ -18,27 +16,9 @@ public class Main {
             }
             PanelPrincipalLogin miVentana = new PanelPrincipalLogin();
     		miVentana.setVisible(true);
-           /* String destinatario = "dacormus@gmail.com"; //A quien le quieres escribir.
-            String asunto = "Correo de prueba enviado desde Java";
-            String cuerpo = "Esta es una prueba de correo...";
-
-            EmailSenderService mail = new EmailSenderService();
-            mail.enviarConGMail(destinatario, asunto, cuerpo);*/
-
     		
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PanelPrincipalLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            System.err.print(ex);
         }
-		
-		
-		//Connection con = ConexionMySQL.conectarMySQL();
-
-		// Query
-		/*
-		 * sSQL =
-		 * "INSERT INTO usuarios (nombre, apellidoUno, apellidoDos, fechaNacimiento, contrasenya, tipoUsuario) VALUES ('edgar', 'dada', 'moreno', '2001-12-16', '123456', 'M');"
-		 * ; // PreparedStatement PreparedStatement pstm = con.prepareStatement(sSQL);
-		 * pstm.execute(); System.out.print("Insertado en SQL");
-		 */
 	}
 }
--- src/util/EmailSenderService.java
+++ src/util/EmailSenderService.java
@@ -2,44 +2,59 @@ package util;
 
 import java.util.Properties;
 import javax.mail.Message;
-import javax.mail.PasswordAuthentication;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.swing.JOptionPane;
 
 public class EmailSenderService {
+	Properties propiedades;
+	Session sesion;
+	String remitente, contrasena, destinatario, asunto, mensaje;
+	MimeMessage mail;
+	Transport transporte;
 
-	public void enviarConGMail(String destinatario, String asunto, String cuerpo) {
-
-		final String username = "movierating3@gmail.com";
-		final String password = "Sox@1234";
-
-		Properties props = new Properties();
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "465");
-
-		Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
-			}
-		});
-
-		try {
-
-			// Define message
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress(username));
-			message.setSubject("asunto");
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress("dacormus@gmail.com"));
-			message.setText("gracias Chuidiang");
-			// Envia el mensaje
-			Transport.send(message);
-		} catch (Exception e) {
-			System.err.print(e);
-		}
+	public void enviarConGMail() {
+		creaPropiedades();
+		creaSesion();
+		enviaMensaje();
 	}
 
+	private void creaPropiedades() {
+		sesion = Session.getDefaultInstance(propiedades);
+		remitente = "movierating3@gmail.com";
+		contrasena = "Sox@1234";
+		destinatario = "dacormo01@hotmail.com";
+		asunto = "Asunto mensaje";
+		mensaje = "Mensaje";
+	}
+
+	private void creaSesion() {
+		propiedades = new Properties();
+		propiedades.put("mail.smtp.auth", true);
+		propiedades.put("mail.smtp.starttls.enable", true);
+		propiedades.put("mail.smtp.host", "smtp.gmail.com");
+		propiedades.put("mail.smtp.port", "587");
+	}
+
+	private void enviaMensaje() {
+		mail = new MimeMessage(sesion);
+		try {
+			mail.setFrom(new InternetAddress(remitente));
+			mail.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));
+			mail.setSubject(asunto);
+			mail.setText(mensaje);
+
+			transporte = sesion.getTransport("smtp");
+			transporte.connect(remitente, contrasena);
+			transporte.sendMessage(mail, mail.getRecipients(Message.RecipientType.TO));
+			transporte.close();
+
+			JOptionPane.showMessageDialog(null, "Correo enviado");
+		} catch (MessagingException ex) {
+			System.err.print(ex);
+		}
+	}
 }
